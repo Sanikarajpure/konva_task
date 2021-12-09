757f42e6252658d17c1ded2d483f5ea411dca34d
Sanikarajpure <sanika.rajpure@gmail.com>
2021-12-09 11:36:42 +0530

    final commit

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect, Fragment } from "react";
 import { Stage, Layer, Rect, Transformer } from "react-konva";
+import "./canvas.css";
 
 const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
   const shapeRef = useRef();
@@ -66,18 +67,46 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
 
 const initialRectangles = [
   {
-    x: 10,
-    y: 10,
+    x: 30,
+    y: 30,
     width: 100,
     height: 100,
     fill: "orange",
     id: "rect1",
+  },
+  {
+    x: 60,
+    y: 50,
+    width: 100,
+    height: 100,
+    fill: "red",
+    id: "rect2",
+  },
+  {
+    x: 40,
+    y: 80,
+    width: 100,
+    height: 100,
+    fill: "yellow",
+    id: "rect3",
   },
 ];
 
 const Canvas_Board = () => {
   const [rectangles, setRectangles] = useState(initialRectangles);
   const [selectedId, selectShape] = useState(null);
+  const stageRef = useRef(null);
+  const imgRef = useRef(null);
+
+  const downloadCanvas = () => {
+    //get base64 uri of the canvas
+    const dataUri = stageRef.current.toDataURL();
+
+    //setting img tag src to dataUri
+
+    imgRef.current.src = dataUri;
+    console.log(dataUri);
+  };
 
   const checkDeselect = (e) => {
     // deselect when clicked on empty area
@@ -88,32 +117,48 @@ const Canvas_Board = () => {
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={checkDeselect}
-      onTouchStart={checkDeselect}
-    >
-      <Layer>
-        {rectangles.map((rect, i) => {
-          return (
-            <Rectangle
-              key={i}
-              shapeProps={rect}
-              isSelected={rect.id === selectedId}
-              onSelect={() => {
-                selectShape(rect.id);
-              }}
-              onChange={(newAttrs) => {
-                const rects = rectangles.slice();
-                rects[i] = newAttrs;
-                setRectangles(rects);
-              }}
-            />
-          );
-        })}
-      </Layer>
-    </Stage>
+    <div className="canvasWrapper">
+      <div
+        onClick={() => {
+          downloadCanvas();
+        }}
+        className="downloadBtn"
+      >
+        Download
+      </div>
+      <div className="canvasBlock">
+        <Stage
+          className="canvasStage"
+          width={800}
+          height={600}
+          onMouseDown={checkDeselect}
+          onTouchStart={checkDeselect}
+          ref={stageRef}
+        >
+          <Layer>
+            {rectangles.map((rect, i) => {
+              return (
+                <Rectangle
+                  key={i}
+                  shapeProps={rect}
+                  isSelected={rect.id === selectedId}
+                  onSelect={() => {
+                    selectShape(rect.id);
+                  }}
+                  onChange={(newAttrs) => {
+                    const rects = rectangles.slice();
+                    rects[i] = newAttrs;
+                    setRectangles(rects);
+                  }}
+                />
+              );
+            })}
+          </Layer>
+        </Stage>
+
+        <img src="" alt="" ref={imgRef} className="pngCanvas" />
+      </div>
+    </div>
   );
 };
 
